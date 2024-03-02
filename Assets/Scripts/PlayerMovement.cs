2b29b0db991bba80bec7d54154543abf81ddb600
Josh O'Bryan <vespergo@gmail.com>
2024-03-02 09:25:30 -0600

    flipping the sprite instead of the rigidbody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    
     public Rigidbody2D playerRigidBody;
     public float speed;
     public float input;
@@ -12,24 +12,41 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce;
 
     public LayerMask groundLayer;
+
     bool isGrounded;
     public Transform feetPosition;
+    public Transform sprite;
     public float groundCheckCircle;
+
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundLayer);
-        if(isGrounded == true && Input.GetButtonDown("Jump"))
+
+        if (isGrounded)
         {
-            playerRigidBody.velocity = Vector2.up * jumpForce;
+            Vector3 rotation = sprite.rotation.eulerAngles;
+            rotation.z = Mathf.Round(rotation.z / 90) * 90;
+            sprite.rotation = Quaternion.Euler(rotation);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                playerRigidBody.velocity = Vector2.up * jumpForce;
+            }
+            
+        }
+        else
+        {
+            sprite.Rotate(Vector3.back * 1);
         }
 
     }
 
+
     // runs at 50 frames/ sec
     private void FixedUpdate()
     {
-        playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);  
+        playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
     }
 }
